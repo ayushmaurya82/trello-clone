92277f9bc506da95d6d885b756da1c504abc5dfc
ayushmaurya82 <ayushmaurya.78@gmail.com>
2024-05-03 13:19:02 +0530

    fixed css for mobile device

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -5,10 +5,7 @@ import StarBorderPurple500OutlinedIcon from "@mui/icons-material/StarBorderPurpl
 
 const Dashboard = () => {
   return (
-    <div
-      className="flex flex-grow-1 overflow-auto"
-      style={{ height: "auto" }}
-    >
+    <div className="flex flex-grow-1 overflow-auto" style={{ height: "auto" }}>
       <div
         className="flex flex-col items-center space-x-3 hidden md:block"
         style={{
@@ -25,22 +22,24 @@ const Dashboard = () => {
           <KeyboardDoubleArrowRightOutlinedIcon className="text-white cursor-pointer" />
         </div>
       </div>
-      <div className="flex sm:block md:block lg:flex-col justify-center">
-        <div className="flex ml-5 md:pt-5 text-white font-bold text-xl space-x-3 invisible md:visible ">
-          <div>Project Team Sprint</div>
-          <div className="bg-gray-600/10 p-1 rounded-md cursor-pointer w-10 h-10 flex items-center justify-center">
-            <StarBorderPurple500OutlinedIcon
-              className="text-warning w-full h-6 w-6"
-              style={{ color: "yellow" }}
-            />
-          </div>
-          <div>|</div>
-          <div className="bg-gray-600/10 p-1 pl-3 pr-3 rounded-md font-normal flex items-center justify-center">
-            ACME Inc.
-          </div>
-          <div>|</div>
-          <div className="bg-gray-600/10 p-1 pl-3 pr-3 rounded-md font-normal flex items-center justify-center">
-            Invite
+      <div className="flex sm:block lg:flex-col justify-center">
+        <div className="flex ml-5 hidden  md:pt-5 text-white font-bold text-xl  md:block ">
+          <div className="flex flex-row space-x-3">
+            <div>Project Team Sprint</div>
+            <div className="bg-gray-600/10 p-1 rounded-md cursor-pointer w-10 h-10 flex items-center justify-center">
+              <StarBorderPurple500OutlinedIcon
+                className="text-warning w-full h-6 w-6"
+                style={{ color: "yellow" }}
+              />
+            </div>
+            <div>|</div>
+            <div className="bg-gray-600/10 p-1 pl-3 pr-3 rounded-md font-normal flex items-center justify-center">
+              ACME Inc.
+            </div>
+            <div>|</div>
+            <div className="bg-gray-600/10 p-1 pl-3 pr-3 rounded-md font-normal flex items-center justify-center">
+              Invite
+            </div>
           </div>
         </div>
         <Board />
